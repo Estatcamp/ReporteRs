d83840b09cb0c7ba310ea50eb6ab6c523f204274
Fazendaaa <lucas.carotta@outlook.com>
2026-06-10 16:50:41 -0300

    Adding import instead of importFrom

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,11 @@
-#' @importFrom gdtools txt_extents
+#' @impor gdtools
 str_extents <- function(x, fontname = "sans", fontsize = 11, bold = FALSE, italic = FALSE, fontfile = "") {
-  gdtools::txt_extents(x, fontname = fontname, fontsize = fontsize, bold = bold, italic = italic, fontfile = fontfile)
+  # Dynamically bind to whatever function this version of gdtools exposes
+  if (exists("txt_extents", envir = asNamespace("gdtools"))) {
+    gdtools::txt_extents(x, fontname = fontname, fontsize = fontsize, bold = bold, italic = italic, fontfile = fontfile)
+  } else if (exists("gfont_extents", envir = asNamespace("gdtools"))) {
+    gdtools::gfont_extents(x, fontname = fontname, fontsize = fontsize, bold = bold, italic = italic, fontfile = fontfile)
+  } else {
+    stop("Compatible font metric function not found in the installed version of gdtools.")
+  }
 }
